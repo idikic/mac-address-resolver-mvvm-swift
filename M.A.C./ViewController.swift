e64bdf9c0addbf3fdc1537c62034a6450fa4eabf
iki <iki@holisticware.com>
2014-07-16 15:42:21 +0200

    Initial commit from local git.

---
+++ M.A.C./ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  M.A.C.
+//
+//  Created by iki on 16/07/14.
+//  Copyright (c) 2014 Iki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ M.A.C.Tests/M_A_C_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  M_A_C_Tests.swift
+//  M.A.C.Tests
+//
+//  Created by iki on 16/07/14.
+//  Copyright (c) 2014 Iki. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class M_A_C_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
