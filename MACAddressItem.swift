a53ef37fa92d92d1f4b536cc477bb453fbc4a456
iki <iki@holisticware.com>
2014-07-17 10:39:47 +0200

    -> data model (modeling)
    - MACAddressItem
    - MACAddressStore (with archiving, NSCoder)

---
+++ MACAddressItem.swift
@@ -0,0 +1,78 @@
+//
+//  MACAddressItem.swift
+//  M.A.C.
+//
+//  Created by iki on 16/07/14.
+//  Copyright (c) 2014 Iki. All rights reserved.
+//
+
+import UIKit
+
+class MACAddressItem: NSObject , NSCoding {
+    
+    let macAddress: String!
+    let company: String?
+    let department: String?
+    let address1: String?
+    let address2: String?
+    let country: String?
+    let locationOfDevice: String?
+    let dateCreated: NSDate!
+    
+    
+    init(macAddress: String) {
+    
+        self.macAddress = macAddress
+        self.dateCreated = NSDate.date()
+        
+        /*
+            In Apple's "Intermediate Swift" video (you can find it in Apple Developer video 
+            resource page https://developer.apple.com/videos/wwdc/2014/), 
+            at about 28:40, it is explicit said that all initializers 
+            in super class must be called AFTER you initialize your instance variables.
+        */
+        
+        super.init()
+        
+        
+        
+    }
+    
+    
+    func description() -> String {
+    
+        var stringDescription: String = "I'am MAC:\(self.macAddress), created at:\(self.dateCreated)"
+        
+        return stringDescription
+    }
+   
+    func encodeWithCoder(aCoder: NSCoder!) {
+    
+        aCoder.encodeObject(macAddress, forKey: "macAddress")
+        aCoder.encodeObject(company, forKey: "company")
+        aCoder.encodeObject(department, forKey: "department")
+        aCoder.encodeObject(address1, forKey: "address1")
+        
+        aCoder.encodeObject(address2, forKey: "address2")
+        aCoder.encodeObject(country, forKey: "country")
+        aCoder.encodeObject(locationOfDevice, forKey: "locationOfDevice")
+        aCoder.encodeObject(dateCreated, forKey: "dateCreated")
+        
+        
+    }
+    
+    init(coder aDecoder: NSCoder!) {
+    
+        self.macAddress = aDecoder.decodeObjectForKey("macAddress") as String
+        self.company = aDecoder.decodeObjectForKey("company") as? String
+        self.department = aDecoder.decodeObjectForKey("department") as? String
+        self.address1 = aDecoder.decodeObjectForKey("address1") as? String
+        self.address2 = aDecoder.decodeObjectForKey("address2") as? String
+        self.country = aDecoder.decodeObjectForKey("country") as? String
+        self.locationOfDevice = aDecoder.decodeObjectForKey("locationOfDevice") as? String
+        self.dateCreated = aDecoder.decodeObjectForKey("dateCreated") as NSDate
+        
+        
+    
+    }
+}
---
+++ MACAddressStore.swift
@@ -0,0 +1,119 @@
+//
+//  MACAddressStore.swift
+//  M.A.C.
+//
+//  Created by iki on 17/07/14.
+//  Copyright (c) 2014 Iki. All rights reserved.
+//
+
+import UIKit
+
+class MACAddressStore: NSObject {
+    
+    class var sharedStore : MACAddressStore {
+    
+        struct Singleton {
+        
+            // lazily initiated, thread-safe from "let"
+            static let instance = MACAddressStore()
+            
+        }
+        return Singleton.instance
+    }
+    
+    
+    var _privateItems = [MACAddressItem]()
+    
+    // The allItems property can't be changed by other objects
+    var allItems: [MACAddressItem] {
+    
+        return _privateItems
+    
+    }
+    
+    
+    init() {
+    
+        super.init()
+        
+        let path = itemArchivedPath
+        
+        // Returns "nil" if there is no file at the path
+        let unarchivedItems : AnyObject! = NSKeyedUnarchiver.unarchiveObjectWithFile(path)
+        
+        // If there were archived items saved, set _privateItems for the shared store equal to that
+        if unarchivedItems {
+        
+            _privateItems = unarchivedItems as Array<MACAddressItem>
+        
+        }
+        
+        delayOnMainQueueFor(numberOfSeconds: 0.1, action: {
+        
+            assert(self === MACAddressStore.sharedStore, "Only one instance of ItemStore allowed!")
+            
+        })
+        
+        
+    }
+    
+    func createItem(macAddress: String!) -> MACAddressItem {
+    
+        let item = MACAddressItem(macAddress: macAddress)
+        _privateItems.append(item)
+        
+        return item
+    
+    }
+    
+    func removeItem(macAddressItem: MACAddressItem!) {
+    
+        for (index, element) in enumerate(_privateItems) {
+        
+            if element === macAddressItem {
+            
+                _privateItems.removeAtIndex(index)
+            }
+        
+        }
+    }
+    
+    var itemArchivedPath: String {
+    
+        // Create a filepath for archiving
+        let documentDirectories = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory,
+                                                                        NSSearchPathDomainMask.UserDomainMask, true)
+            
+        // Get the one document directory from that list
+        let documentDirectory = documentDirectories[0] as String
+            
+        // append with the items.archive file name, then return
+        return documentDirectory.stringByAppendingPathComponent("items.archive")
+    
+    
+    }
+    
+    func saveChanges() -> Bool {
+    
+        let path = itemArchivedPath
+        
+        // Return "true" on success
+        return NSKeyedArchiver.archiveRootObject(_privateItems, toFile: path)
+    }
+    
+    func delayOnMainQueueFor(numberOfSeconds delay:Double, action closure:()->()) {
+       
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+           
+            dispatch_get_main_queue()) {
+                closure()
+        }
+        
+    }
+    
+   
+}
